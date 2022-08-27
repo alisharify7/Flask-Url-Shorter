7d94e349fcc00f036255bd34603d167c8cbe6725
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2022-08-27 17:46:34 +0430

    fix class

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
 db_name="app.db"
+
 class Config:
-    DEBUG=True
-    FLASK_DEBUG=True
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{db_name}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-
+    
 
 class Development(Config):
-    SECRET_KEY = "hello world! :) so safe"
+    secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+    DEBUG=True
+        FLASK_DEBUG=True
+
+
+class Production(Config):
+    DEBUG=False
+    FLASK_DEBUG=False
