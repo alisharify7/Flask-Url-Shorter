afb934b4dcc37dcf951d08ceb81d55cc18a595e5
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-08-27 15:11:45 +0430

    add flash message

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import (
     Flask,
     redirect,
     render_template,
+    request,
     flash
     )
 from config import Development
@@ -10,6 +11,12 @@ app = Flask(__name__)
 app.config.from_object(Development)
 
 
-@app.route("/")
+@app.route("/",methods=["GET", "POST"])
 def index():
+    print(request.form)
+    flash("hello From Server","error")
     return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0',port=8080,debug=True)
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ class Config:
     FLASK_DEBUG=True
     SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
 class Development(Config):
