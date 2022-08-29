c870694e4401fe4e5c9954721d50f0dafc94b203
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2022-08-29 15:56:45 +0430

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 db_name="app.db"
-# set on default in flask 
-host="http://127.0.0.1:8080"
+port=8080 
+host=f"http://127.0.0.1:{port}"
 
 class Config:
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{db_name}"
