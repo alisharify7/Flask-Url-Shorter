ac35106761b9329d41bf086a9e93f880f19a5e7f
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-09-03 14:24:32 +0430

    update helpers (remover/validate url function added)

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -7,6 +7,7 @@ def response(message):
     flash(message)
     return render_template("index.html")
 
+
 def create_random_url():
     import string
     import random
@@ -19,3 +20,36 @@ def create_random_url():
         random.shuffle(string_out)
 
     return "".join(string_out)
+
+
+
+def url_remover(url):
+    """
+    This Function remove all not important Part From Url
+    """
+    www = True    
+    https = True    
+    while(www):
+        if "www" in url:
+            url = url.replace("www","")
+            url = url.strip()
+            wwww = False
+    while(https):
+        if "https" in url:
+            url = url.replace("https","")
+            url = url.strip()
+            https= False
+        if "http" in url:
+            url = url.replace("http","")
+            url = url.strip()
+            https= False
+
+
+def validate_url(url):
+    # TODO 
+    # [x] check and validate 55
+    # [0] handel real address
+    # [0] clean input string
+    return url_remover(url)
+
+        
