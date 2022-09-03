c9308dcb4a37bb6d8cc80a8195a73632a5463282
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2022-09-03 07:38:01 +0430

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ host=f"http://127.0.0.1:5000"
 class Config:
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{db_name}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # default secret key From flask own doc
     SECRET_KEY = b'_5#y2L"F4Q8z\n\xec]/'
     
 
